5c2c6f80dbc32e70ce8e76eec08b9ff3f53ec68b
Adedotun Alausa <adedotunalausa@gmail.com>
2021-06-21 14:40:58 +0100

    Set up project

---
+++ src/main/java/com/cova/covaquizapp/CovaQuizAppApplication.java
@@ -0,0 +1,13 @@
+package com.cova.covaquizapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovaQuizAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovaQuizAppApplication.class, args);
+    }
+
+}
